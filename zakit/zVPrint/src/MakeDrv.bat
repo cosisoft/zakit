75b6d47b1bbe606851f3a4b846f7a4dfbf748e9b
zero.flamy <zero.flamy@gmail.com>
2008-09-04 17:00:44 +0000

    add MakeDrv.bat

---
+++ zakit/zVPrint/src/MakeDrv.bat
@@ -0,0 +1,27 @@
+@echo off
+if "%1" == "" goto usage
+
+if "%1" == "." (
+	set TmpDir="%CD%"
+) else (
+	set TmpDir="%1"
+)
+
+set DDKROOT=N:\WINDDK\3790
+if not exist %DDKROOT%\bin\setenv.bat goto usage
+
+CALL %DDKROOT%\bin\setenv.bat %DDKROOT% %2 %3
+
+cd /d %TmpDir%
+
+build.exe -b -w -g %4 %5 %6 %7 %8
+
+goto exit
+
+:usage
+echo
+echo usage: MakeDrv Source_Dir w2k/wxp [free/checked]or[f/c] [build options]
+echo 
+
+:exit
+@echo on
